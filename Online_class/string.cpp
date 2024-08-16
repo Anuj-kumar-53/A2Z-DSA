3aab7be09cbc2dcb0c81b00cf8ce594ef21932a9
Anuj kumar <anujksah123@gmail.com>
2024-08-16 23:01:18 +0530

    stings basics

---
+++ Online_class/string.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string n;
+    cin.ignore(); // ignores only the first latter
+    cin.ignore(5); // ignores five characters
+    getline(cin,n);
+    cout<<n;
+}
