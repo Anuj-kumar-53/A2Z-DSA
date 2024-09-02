0440665f923201e73f6ce93b6e16fe0aa5ed73d3
Anuj <anujksah123@gmail.com>
2024-09-02 22:34:00 +0530

    min day required to form a bukey

---
+++ sorting_algo/no_of_bukeys_from_of_m_k.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+int ispossible(int arr[],int n,int m,int k,int day){
+    int count =0;
+    int bukey = 0;
+    for(int i=0;i<n;i++){
+        if(arr[i]<=day){
+            //that means they will bloom
+            count++;
+            if(count == k){
+                bukey++;
+                count = 0;
+            }
+        }
+        else{
+            count =0;
+        }
+
+    }
+    return bukey>=m;
+}
+int main(){
+    int n;cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int low = INT_MAX;
+    int high = INT_MIN;
+    for(int i=0;i<n;i++){
+        low = min(arr[i],low);
+        high = max(arr[i],high);
+    }
+    int m;cout<<"the no of bukey";
+    cin>>m;
+    int k;
+    cout<<"no of element in one bukey";
+    cin>>k;
+    if(m*k>n){ 
+
+    cout<<"-1"; return 0;
+    } 
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(ispossible(arr,n,m,k,mid)){
+            high = mid-1;
+        }
+        else low = mid+1;
+    }
+    cout<<low;
+    return 0;
+}
